f5d8e3a78a88cc17b553f522136940c5b4f5d850
duyd486 <90164590+duyd486@users.noreply.github.com>
2025-09-05 23:29:33 +0700

    add input + move

---
+++ Assets/MyGame/Scripts/Player/GameInput.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInput playerInputAction;
+
+
+
+    private void Awake()
+    {
+        Instance = this;
+
+        playerInputAction = new PlayerInput();
+
+        playerInputAction.Enable();
+
+
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
---
+++ Assets/MyGame/Scripts/Player/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 1f;
+    [SerializeField] private bool isWalking = true;
+
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        Transform cameraTransform = Camera.main.transform;
+        Vector3 inputDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 moveDir = (cameraForward * inputDir.z + cameraRight * inputDir.x).normalized;
+
+        transform.position += moveDir * moveDistance;
+
+        isWalking = moveDir != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+    }
+
+}
